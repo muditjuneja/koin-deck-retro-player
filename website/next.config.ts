6d5fe09425fc3953e2cd426ca652e1d1445c0490
Mudit <muditjuneja@outlook.com>
2025-12-11 00:17:57 +0530

    removing the base path to move to

--- website/next.config.ts
+++ website/next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/koin', // Remove when using koin.js.org
   images: {
     unoptimized: true,
   },
